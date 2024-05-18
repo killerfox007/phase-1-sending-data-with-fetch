c941a9ea6ef3a8ae41a9d71a73b786a902bedacd
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-17 21:01:20 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your code here
+
+const getBody = document.querySelector("body")
+
+
+function submitData(name, email){
+const user = {
+    name: name,
+    email: email
+}
+
+
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    body: JSON.stringify(user)
+})
+.then(response => response.json())
+.then(data => {
+    getBody.append(data.id)
+    console.log(data.id)
+})
+.catch(error => {
+    getBody.append(error.message)
+    console.log(error.message)
+})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
